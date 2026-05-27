f478321b8dd60c05dfeb7551c215fdd0d27521ad
MatheusAPapa <matheus.apb@puccampinas.edu.br>
2026-05-26 21:14:23 -0300

    atualizando projeto

--- projeto-bd.sql
+++ projeto-bd.sql
@@ -1,20 +1,48 @@
-CREATE DATABASE dentistas;
-USE dentitas;
+CREATE DATABASE IF NOT EXISTS clinica_passini;
+USE clinica_passini;
 
-CREATE TABLE clientes(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-telefone VARCHAR(50) NOT NULL,
-cpf VARCHAR(11) UNIQUE NOT NULL
+-- tabela de todos os convênios que os médicos da clínas atendem
+CREATE TABLE convenios(
+id_convenio INT AUTO_INCREMENT PRIMARY KEY,
+nome_convenio VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE agenda(
-id INT AUTO_INCREMENT PRIMARY KEY,
-data DATETIME
-);
-
+-- tabela dos profissionais
 CREATE TABLE profissionais(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-experiencia VARCHAR(100) NOT NULL
+id_profissional INT AUTO_INCREMENT PRIMARY KEY,
+nome_profissional VARCHAR(100) NOT NULL,
+cpf_profissional VARCHAR(11) UNIQUE NOT NULL,
+cro VARCHAR(20) UNIQUE NOT NULL,
+email_profissional VARCHAR(100) UNIQUE NOT NULL,
+telefone_profissional VARCHAR(18) UNIQUE NOT NULL,
+status BOOLEAN DEFAULT TRUE 
+);
+
+-- tabela para referenciar o profissional e o convênio que ele atende
+CREATE TABLE profissionais_convenio(
+id_vinculo INT AUTO_INCREMENT PRIMARY KEY,
+id_profissional INT NOT NULL,
+id_convenio INT NOT NULL,
+FOREIGN KEY (id_profissional) REFERENCES profissionais(id_profissional),
+FOREIGN KEY (id_convenio) REFERENCES convenios(id_convenio)
+);
+
+-- tabela dos clientes
+CREATE TABLE clientes(
+id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+nome_cliente VARCHAR(100) NOT NULL,
+telefone_cliente VARCHAR(50) NOT NULL,
+cpf_cliente VARCHAR(11) UNIQUE NOT NULL,
+id_convenio INT,
+FOREIGN KEY (id_convenio) REFERENCES convenios(id_convenio)
+);
+
+-- tabela de agendamento das consultas
+CREATE TABLE agenda(
+id_consulta INT AUTO_INCREMENT PRIMARY KEY,
+data_consulta DATETIME,
+id_cliente INT NOT NULL,
+id_profissional INT NOT NULL,
+FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+FOREIGN KEY (id_profissional) REFERENCES profissionais(id_profissional)
 );
