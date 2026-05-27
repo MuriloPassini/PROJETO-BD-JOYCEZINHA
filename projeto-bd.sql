c21c7ff5993ccfa42a5cc6a5e6bc6ea684b12ce7
Salazar2210 <rafael.sac@puccampinas.edu.br>
2026-05-27 11:01:59 -0300

    aaaaaaaaa

--- projeto-bd.sql
+++ projeto-bd.sql
@@ -1,6 +1,13 @@
 CREATE DATABASE IF NOT EXISTS clinica_passini;
 USE clinica_passini;
 
+-- tabela de usuários
+CREATE TABLE usuario (
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+user_hash VARCHAR(50) NOT NULL,
+password_hash VARCHAR(50) NOT NULL
+);
+
 -- tabela de todos os convênios que os médicos da clínas atendem
 CREATE TABLE convenios(
 id_convenio INT AUTO_INCREMENT PRIMARY KEY,
@@ -11,10 +18,13 @@ nome_convenio VARCHAR(50) NOT NULL
 CREATE TABLE profissionais(
 id_profissional INT AUTO_INCREMENT PRIMARY KEY,
 nome_profissional VARCHAR(100) NOT NULL,
+sobrenome_profissional VARCHAR(100) NOT NULL,
+nome_social_profissional VARCHAR(100),
 cpf_profissional VARCHAR(11) UNIQUE NOT NULL,
 cro VARCHAR(20) UNIQUE NOT NULL,
 email_profissional VARCHAR(100) UNIQUE NOT NULL,
 telefone_profissional VARCHAR(18) UNIQUE NOT NULL,
+pagamento VARCHAR(50) NOT NULL, ****
 status BOOLEAN DEFAULT TRUE 
 );
 
@@ -27,13 +37,29 @@ FOREIGN KEY (id_profissional) REFERENCES profissionais(id_profissional),
 FOREIGN KEY (id_convenio) REFERENCES convenios(id_convenio)
 );
 
+-- tabela de todos os serviços que é feito na clínica
+CREATE TABLE servicos (
+id_servico INT AUTO_INCREMENT PRIMARY KEY,
+nome_servico VARCHAR(70),
+descricao_servico VARCHAR(150),
+preco_servico DECIMAL(5,2)
+);
+
+-- tabela para relacionar os prifissionais e o serviço que eles fazem
+CREATE TABLE servicosXfuncionarios (
+id_especialidade INT AUTO_INCREMENT PRIMARY KEY
+);****
+
 -- tabela dos clientes
 CREATE TABLE clientes(
 id_cliente INT AUTO_INCREMENT PRIMARY KEY,
 nome_cliente VARCHAR(100) NOT NULL,
+sobrenome_cliente VARCHAR(100) NOT NULL,
+nome_social_cliente VARCHAR(100),
 telefone_cliente VARCHAR(50) NOT NULL,
 cpf_cliente VARCHAR(11) UNIQUE NOT NULL,
 id_convenio INT,
+termo_servico BOOLEAN NOT NULL,
 FOREIGN KEY (id_convenio) REFERENCES convenios(id_convenio)
 );
 
@@ -46,3 +72,7 @@ id_profissional INT NOT NULL,
 FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
 FOREIGN KEY (id_profissional) REFERENCES profissionais(id_profissional)
 );
+
+-- fazer lista de servico por profisssional[especialidades] alterando a fk na agenda, especialidades, LGPD , AUDITORIA, 
+-- PAGAMENTO, preço por serviço
+
