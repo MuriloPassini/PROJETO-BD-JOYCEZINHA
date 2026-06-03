3f77d33264ff16a07f6b571f9414fd835804427f
MatheusAPapa <matheus.apb@puccampinas.edu.br>
2026-06-02 23:10:46 -0300

    script de apoio

---
+++ script_apoio.sql
@@ -0,0 +1,14 @@
+use clinica_passini;
+
+SELECT * FROM convenios;
+SELECT * FROM clientes;
+SELECT * FROM profissionais;
+SELECT * FROM usuario;
+SELECT * FROM profissionais_convenio;
+SELECT * FROM servicos;
+SELECT * FROM especialidadeXprofissional;
+SELECT * FROM agenda;
+SELECT * FROM pagamentos;
+SELECT * FROM pagamento_servicos;
+SELECT * FROM auditoria_log;
+SELECT * FROM auditoria_acesso;
